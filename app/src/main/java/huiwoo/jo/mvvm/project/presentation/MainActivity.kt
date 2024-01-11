86e23b17ddcac346beef1c739c27d881a4893e02
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-01-11 14:27:04 +0900

    feature : 패키지 구조에 맞춘 파일 추가

---
+++ app/src/main/java/huiwoo/jo/mvvm/project/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package huiwoo.jo.mvvm.project.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import huiwoo.jo.mvvm.project.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/huiwoo/jo/mvvm/project/presentation/detail/DetailViewModel.kt
@@ -0,0 +1,4 @@
+package huiwoo.jo.mvvm.project.presentation.detail
+
+class DetailViewModel {
+}
