20ab824553f9caec9031817c7198c12a3cc1e68a
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-01-22 18:17:13 +0900

    chore : MainViewModel 수정

--- app/src/main/java/huiwoo/jo/mvvm/project/presentation/news/MainViewModel.kt
+++ app/src/main/java/huiwoo/jo/mvvm/project/presentation/news/MainViewModel.kt
@@ -1,28 +1,40 @@
 package huiwoo.jo.mvvm.project.presentation.news
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
 import huiwoo.jo.mvvm.project.data.model.News
 
-class MainViewModel: ViewModel() {
+@SuppressLint("SuspiciousIndentation")
+class MainViewModel : ViewModel() {
     private val _newsList = MutableLiveData<ArrayList<News>>()
-    val newsList : LiveData<ArrayList<News>>
+    val newsList: LiveData<ArrayList<News>>
         get() = _newsList
 
-    //data for test
-    private val title = "news"
-    private val author = "author"
-    private val time = "2024-01-18"
-    private val elapsed = "7hour ago"
-    private val content = "test"
-    private val url = "img.url" //url 수정 필요
-    private val items = arrayListOf(
-        News(title,author,url, content, time, elapsed)
+    private val items: ArrayList<News> = arrayListOf(
+        News(
+            "news title",
+            "author",
+            "https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg",
+            "test",
+            "2024-01-18",
+            "7hour ago"
+        ),
+        News(
+            "news title2",
+            "author",
+            "https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg",
+            "test",
+            "2024-01-18",
+            "7hour ago"
+        )
     )
 
-    init{
+    init {
         _newsList.value = items
     }
+
 }
 
